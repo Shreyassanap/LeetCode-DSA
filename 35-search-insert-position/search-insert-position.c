d78b040b22cd65a52bdf36f4016d56ede29f886e
Shreyas Sharad Sanap <156312094+Shreyassanap@users.noreply.github.com>
2026-07-29 14:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (70.16%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.c
@@ -0,0 +1,23 @@
+int searchInsert(int* nums, int numsSize, int target) {
+
+    int low=0,high=numsSize-1,mid=0;
+    
+    while(low<=high)
+    {
+        mid=low+((high-low)/2);
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        else
+        {
+            if(nums[mid]>target)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+
+    }
+    return high+1;
+    
+}
