4ddedf264e6a600bc14906912f11ff51f4950623
Shreyas Sharad Sanap <156312094+Shreyassanap@users.noreply.github.com>
2026-07-30 22:36:12 +0530

    Time: 296 ms (5.19%) | Memory: 88.2 MB (5.16%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+
+        
+    unordered_map<char,int> mpp;
+    for(int i=0;i<s.length();i++)
+        mpp[s[i]]++;
+    
+    vector<char> ans1;
+    vector<char> mid;
+    vector<char> ans2;
+
+    sort(s.begin(),s.end());
+    int flag=0;
+    int count=0;
+    for(int i=0;i<s.length();i++)
+    {
+        if(mpp[s[i]]%2==1 && count==0){
+            mid.push_back(s[i]);
+            count++;
+        }
+        else
+        {
+            if(flag==0)
+            {
+                ans1.push_back(s[i]);
+                flag=1;
+            }
+            else
+            {
+                ans2.push_back(s[i]);
+                flag=0;
+            }
+
+        }
+    }
+
+    reverse(ans2.begin(),ans2.end());
+
+    ans1.insert(ans1.end(),mid.begin(),mid.end());
+    ans1.insert(ans1.end(),ans2.begin(),ans2.end());
+
+    string es(ans1.begin(),ans1.end());
+    return es;
+    
+
+        
+
+        
+        
+    }
+};
