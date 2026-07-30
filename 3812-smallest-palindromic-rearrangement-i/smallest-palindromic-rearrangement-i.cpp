b707060a2f816ead0ec8fe310230c6ec9b2a857a
Shreyas Sharad Sanap <156312094+Shreyassanap@users.noreply.github.com>
2026-07-30 23:07:15 +0530

    Time: 256 ms (5.19%) | Memory: 79 MB (14.24%) - LeetSync

--- 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -4,23 +4,36 @@ public:
 
         unordered_map<char,int> mpp;
 
-        for(char c : s)
-            mpp[c]++;
-
-        string left = "";
-        string mid = "";
-
-        for(char c = 'a'; c <= 'z'; c++)
+        for(int i=0;i<s.length();i++)
+            mpp[s[i]]++;
+        
+        vector<char> ans;
+        int count=0;
+        vector<char>mid;
+        sort(s.begin(),s.end());
+        for(int i=0;i<s.length();i++)
         {
-            left.append(mpp[c] / 2, c);
-
-            if(mpp[c] % 2 == 1)
-                mid += c;
+            if(mpp[s[i]]%2==1 && count==0){
+                mid.push_back(s[i]);
+                count++;
+            }
+            else
+                ans.push_back(s[i++]);
         }
 
-        string right = left;
-        reverse(right.begin(), right.end());
+       
 
-        return left + mid + right;
+        string b(ans.begin(),ans.end());
+        ans.insert(ans.end(),mid.begin(),mid.end());
+
+        string a(ans.begin(),ans.end());
+
+        reverse(b.begin(),b.end());
+
+        return a+b;
+        
+
+
+        
     }
 };
