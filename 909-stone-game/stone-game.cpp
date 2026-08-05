b239812539cdc90909cbdbe18da9f40a06779f67
Shreyas Sharad Sanap <156312094+Shreyassanap@users.noreply.github.com>
2026-08-06 00:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.59%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,9 +1,32 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
 
-        
+        int one=0,two=0;
+        int i=0,j=piles.size()-1;
+        while(i<=j)
+        {
+            if(piles[i]>piles[j])
+            {
+                one=one+piles[i];
+                i++;
+
+                two=two+piles[j];
+                j--;
+            }
+            else
+            {
+                one=one+piles[j];
+                j--;
+                two=two+piles[i];
+                i++;
+            }
+
+        }
+
+        if(one>two)
+            return true;
+        return false;
         
     }
 };
