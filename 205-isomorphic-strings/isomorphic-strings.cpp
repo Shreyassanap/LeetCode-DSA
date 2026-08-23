8e2b6fe02b848ac9dace815465533ca921ea5f5f
Shreyas Sharad Sanap <156312094+Shreyassanap@users.noreply.github.com>
2026-08-24 01:04:41 +0530

    Time: 3 ms (40.52%) | Memory: 9.4 MB (58.77%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+
+        if(s.length()!=t.length())
+            return false;
+
+        unordered_map<char,char>mpp1;
+        unordered_map<char,char>mpp2;
+
+        for(int i=0;i<s.length();i++)
+        {
+            mpp1[s[i]]=t[i];
+            mpp2[t[i]]=s[i];
+        }
+
+        if(mpp1.size()!=mpp2.size())
+            return false;
+        int i=0;
+        while(i!=s.length())
+        {
+            s[i]=mpp1[s[i]];
+            i++;
+        }
+
+        if(s==t)
+            return true;
+        return false;
+
+
+
+        
+    }
+};
